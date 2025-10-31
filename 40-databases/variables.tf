fdeb6bf743ab16b03c306a29857ce03382e86629
chandu kumar <chandukuttuboina@gmail.com>
2025-10-31 23:00:21 +0530

    session

---
+++ 40-databases/variables.tf
@@ -0,0 +1,28 @@
+variable "project_name" {
+    type = string
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+  
+}
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        #frontend load balancer
+        "frontend_alb",
+        #Backend ALB
+        "backend_alb"
+    ]
+}
