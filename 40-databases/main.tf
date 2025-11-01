eb72015d0d112950030ee4295e10033efcfc8c35
chandu kumar <chandukuttuboina@gmail.com>
2025-11-01 13:55:17 +0530

    session

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -33,7 +33,7 @@ resource "terraform_data" "mongodb" {
     inline =  [
         "chmod +x /tmp/bootstrap.sh",
         # "sudo sh /tmp/bootstrap.sh"
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh"
     ]
     
   }
