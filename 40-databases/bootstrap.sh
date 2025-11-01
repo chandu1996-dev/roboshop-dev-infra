99fd9922155773132a675faeab379a8150a54a56
chandu kumar <chandukuttuboina@gmail.com>
2025-11-01 18:54:20 +0530

    inv

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,2 +1,5 @@
+component=$1
+
 dnf install ansible -y
-ansible-pull -U https://github.com/chandu1996-dev/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/chandu1996-dev/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -18,11 +18,15 @@ data "aws_ami" "joindevops" {
         values = ["hvm"]
     }
 }
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+}
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
-data "aws_ssm_parameter" "database_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+
+data "aws_ssm_parameter" "redis_sg_id_ids" {
+  name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
