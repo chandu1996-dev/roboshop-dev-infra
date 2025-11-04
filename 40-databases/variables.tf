11f7a3852198e0b54a296005552c2a00e00c4879
chandu kumar <chandukuttuboina@gmail.com>
2025-11-04 22:52:16 +0530

    inv

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -26,3 +26,11 @@ variable "sg_names" {
         "backend_alb"
     ]
 }
+
+variable "zone_id" {
+    default = "Z00716421P5ZXLU5W7GIT"
+}
+
+variable "domain_name" {
+    default = "born96.fun"
+}
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -13,3 +13,46 @@ resource "aws_instance" "catalogue" {
         }
     )
 }
+
+
+# Connect to instance using remote-exec provisioner through terraform_data 
+
+resource "aws_instance" "catalogue" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.catalogue_sg_id]
+    subnet_id = local.private_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue" # roboshop-dev-redis
+        }
+    )
+}
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  # terraform copies this file to catalogue server
+  provisioner "file" {
+    source = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/catalogue.sh",
+        "sudo sh /tmp/catalogue.sh catalogue"
+    ]
+  }
+}
