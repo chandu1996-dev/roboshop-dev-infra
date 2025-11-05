2e20ccc0f431fb48480c8d1d129525df0e52307f
chandu kumar <chandukuttuboina@gmail.com>
2025-11-06 01:17:17 +0530

    inv

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -6,3 +6,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z00716421P5ZXLU5W7GIT"
+}
+
+variable "domain_name" {
+    default = "born96.fun"
+}
