a70928d21fe875ea4d910dc71da5da49046469b8
chandu kumar <chandukuttuboina@gmail.com>
2025-11-06 12:32:04 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -6,7 +6,7 @@ locals {
 
   ami_id= data.aws_ami.joindevops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  # vpc_id = data.aws_ssm_parameter.vpc_id.value
     
     common_tags = {
         Project = var.project_name
